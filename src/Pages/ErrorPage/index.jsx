1cb43e1c854c5837ae456c1d35eecade0207af5a
Halima-DERRAHI <123747796+Halima-DERRAHI@users.noreply.github.com>
2024-01-01 23:15:46 +0100

    errorPage

--- src/Pages/ErrorPage/index.jsx
+++ src/Pages/ErrorPage/index.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+import styles from './ErrorPage.module.css'
 
 function ErrorPage() {
-  return (
-    <p>Error Page</p>
-  );
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>404</h1>
+            <div className={styles.textContainer}>
+                <p className={styles.text}>Oups! La page que </p>
+                <p className={styles.text}>vous demandez n'existe pas.</p>
+            </div>
+            <Link to="/" className={styles.link}>Retourner sur la page d’accueil</Link>
+        </div>
+    )
 }
 
-export default ErrorPage;
+export default ErrorPage
